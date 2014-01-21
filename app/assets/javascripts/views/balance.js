8c77e569c8a8856533ebe96bfd2e992e9f455eb9
Ian Purton <ian.purton@gmail.com>
2014-01-21 13:44:42 +0000

    format balance

--- app/assets/javascripts/views/balance.js
+++ app/assets/javascripts/views/balance.js
@@ -1,12 +1,12 @@
 window.App.Views.Balance = Backbone.View.extend({
   
   initialize: function() {
-    App.on('login', this.updateBalance, this);
+    App.on('login', this.updateUserDetails, this);
     App.on('updateBalance', this.updateBalance, this);
   },
 
   updateBalance: function() {
-    this.$el.val(App.user.get('balance'));
+    this.$el.val(+App.user.get('balance').toFixed(8));
   }
   
 });
