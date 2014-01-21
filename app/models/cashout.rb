e642330fd0a76e03ba6d185693cc8bd6b2d597b6
Kieran Johnson <kieran@invisiblelines.com>
2014-01-21 14:04:11 +0000

    log response

--- app/models/cashout.rb
+++ app/models/cashout.rb
@@ -5,7 +5,8 @@ class Cashout
     uri        = URI.parse("https://blockchain.info/merchant/#{pkey}/payment")
     response   = Net::HTTP.post_form(uri, attributes)
 
-    Rails.logger.info "Payment of #{amount} sent to #{recipient}"
+    Rails.logger.error "!!! - Payment of #{amount} sent to #{recipient}"
+    Rails.logger.error "!!! - #{response.body}"
 
     response.body
   end
