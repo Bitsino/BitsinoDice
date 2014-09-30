7e299623016a3e23b7c70b4ceb083635c677c328
Ian Purton <ian.purton@gmail.com>
2014-09-30 14:54:54 +0200

    Keep internal balance

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930085512) do
+ActiveRecord::Schema.define(version: 20140930120939) do
 
   create_table "bets", force: true do |t|
     t.integer  "user_id"
@@ -39,12 +39,12 @@ ActiveRecord::Schema.define(version: 20140930085512) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                           default: "", null: false
+    t.string   "encrypted_password",                              default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                                   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140930085512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.decimal  "balance",                precision: 15, scale: 8
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
