e2cd2a3c0ae46de9e07704799ea159e555e5e36d
Ian Purton <ianpurton@ians-air.fritz.box>
2014-10-13 10:39:54 +0200

    Auto register users

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -8,6 +8,17 @@ window.App.Models.User = Backbone.Model.extend({
 
   updateUserBalance: function() {
     App.trigger('updateBalance');
+  },
+  
+  randomString: function() {
+  	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  	var string_length = 8;
+  	var randomstring = '';
+  	for (var i=0; i<string_length; i++) {
+  		var rnum = Math.floor(Math.random() * chars.length);
+  		randomstring += chars.substring(rnum,rnum+1);
+  	}
+  	return randomstring;
   }
 
 });
