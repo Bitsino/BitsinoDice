2ab526a89d03d91e56437fbd4912a8300b1a86b1
Ian Purton <ian.purton@gmail.com>
2014-10-13 10:48:59 +0200

    Switch assets to debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Primedice::Application.configure do
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.debug = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
