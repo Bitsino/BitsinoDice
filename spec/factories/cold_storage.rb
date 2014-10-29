f4e3c85e4da761fe76b2f4896ac2d08d41eb8043
Ian Purton <ian.purton@gmail.com>
2014-10-29 09:53:37 +0100

    Login and registration spec.

---
+++ spec/factories/cold_storage.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  
+  factory :cold_storage do
+    mpk 'xpub69GZWTQPtwQRriHyYuYJpDgAUrHHRD8ksBbQ61QpY1CbSUrcW7udYcZ1YLuLVtSQx9xW5QApiGidDfmFVLEz4Lep3AoCGD2HQmfvXwH1GMt'
+    fund_address '3GzGsZ5zFWsFR5LU8TYntptkZqvZrPWzw5'
+    block 0
+    sweep_block 0
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  
+  factory :user do
+    email "bitdice@bitdice.io"
+    password "qtw25cct5tjhpppw"
+    password_confirmation {"qtw25cct5tjhpppw"}
+  end
+end
