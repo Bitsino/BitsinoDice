5122db7d065eb7bcb59c6b2fa83c5ac2a43f40b8
Ian Purton <ian.purton@gmail.com>
2014-11-22 20:08:21 +0000

    Force ssl again.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   
-  scope defaults: (Rails.env.production? ? { protocol: 'https' } : {}) do
-    devise_for :users
-  end
-  
 
   resources :bets,     only: [ :show, :create ]
   resources :cashouts, only: [ :create ]
